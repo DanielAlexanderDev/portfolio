cec9114773078aa01414a7b0d89abb8a5e5d63ce
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2023-01-10 17:05:10 -0500

    Delete portfolio dynamic path

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/portfolio/:id" element={<p>Project</p>} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Layout>
