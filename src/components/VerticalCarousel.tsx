85c3e458138a2cf638d0dd3f850c46aa60b3bba7
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2023-01-10 17:08:14 -0500

    Add public paths

--- src/components/VerticalCarousel.tsx
+++ src/components/VerticalCarousel.tsx
@@ -1,8 +1,3 @@
-import p1 from "../assets/mern-app.png";
-import p2 from "../assets/weather.png";
-import p3 from "../assets/ecommerce.png";
-import p4 from "../assets/tic-tac-toe.png";
-
 const VerticalCarousel = () => {
   return (
     <div className="md:min-w-[350px] animate-fade-in md:w-1/3 md:h-[500px] max-h-[500px] h-auto min-w-[350px] w-2/3 md:relative border-2  md:flex md:items-center rounded-md md:justify-center py-2 md:px-4 mb-5 md:mb-0">
@@ -10,22 +5,22 @@ const VerticalCarousel = () => {
         <div className="w-full"></div>
         <img
           className={`md:animate-vertical-scroll animate-horizontal-scroll`}
-          src={p1}
+          src={"/mern-app.png"}
           alt="asdq"
         />
         <img
           className={` md:pt-5 md:animate-vertical-scroll animate-horizontal-scroll`}
-          src={p2}
+          src={"/weather.png"}
           alt="sdq"
         />
         <img
           className={` md:pt-5 md:animate-vertical-scroll animate-horizontal-scroll`}
-          src={p3}
+          src={"/ecommerce.png"}
           alt="dqds"
         />
         <img
           className={` md:pt-5 md:animate-vertical-scroll animate-horizontal-scroll`}
-          src={p4}
+          src={"/tic-tac-toe.png"}
           alt="dqd"
         />
       </div>
