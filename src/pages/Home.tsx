a1495aa2e9a17057fd30b5a654feeea59a56318b
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2023-01-10 17:08:30 -0500

    Add dowload cv feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,31 @@ import Hero from "../components/Hero";
 import PortfolioOverview from "../components/PortfolioOverview";
 import { SiGithub, SiInstagram, SiLinkedin, SiTwitter } from "react-icons/si";
 import CV from "../Daniel-Llumiquinga-CV.pdf";
+import { FC } from "react";
 
-const Home = () => {
+const Home: FC = () => {
+  const downloadFile = (url: string, fileName: string) => {
+    fetch(url, {
+      method: "get",
+      mode: "no-cors",
+      referrerPolicy: "no-referrer",
+    })
+      .then((res) => res.blob())
+      .then((res) => {
+        const aElement = document.createElement("a");
+        aElement.setAttribute("download", fileName);
+        const href = URL.createObjectURL(res);
+        aElement.href = href;
+        aElement.setAttribute("target", "_blank");
+        aElement.click();
+        URL.revokeObjectURL(href);
+      });
+  };
   return (
-    <main className="dark:bg-gradient-to-r from-zinc-900 via-black to-zinc-900 bg-white dark:text-white">
-      <div className={`flex flex-col items-center animate-fade-in`}>
+    <main className="bg-gradient-to-r from-zinc-900 via-black to-zinc-900 text-white">
+      <div
+        className={`flex flex-col items-center pt-12 md:pt-0 animate-fade-in`}
+      >
         <Hero />
         <div className="flex flex-col md:flex-row md:justify-evenly w-full flex-wrap items-center justify-center gap-x-3">
           <div className=" flex justify-center items-center min-w-[350px] w-1/4 my-8 ">
@@ -28,7 +48,10 @@ const Home = () => {
           </div>
           <TechList />
         </div>
-        <button className="bg-[#FEFBE8] rounded-md text-black py-2 px-3 mb-5 animate-fade-in transition duration-700  hover:bg-slate-800 hover:text-[#FEFBE8]">
+        <button
+          onClick={() => downloadFile(CV, "Daniel Llumiquinga CV.pdf")}
+          className="bg-[#FEFBE8] rounded-md text-black py-2 px-3 mb-5 animate-fade-in transition duration-700  hover:bg-slate-800 hover:text-[#FEFBE8]"
+        >
           Download CV
         </button>
 
